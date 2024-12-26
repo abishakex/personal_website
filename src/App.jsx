cc54399debdc3e7b624aa1b6907f85a38db255de
Abishek <a7abishek@gmail.com>
2024-12-26 18:52:58 +0300

    fixed ML path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import ML from './components/ml';
+import ML from './components/ML';
 import Reads from './components/Reads';
 import BlogPost from './components/BlogPost';  // Make sure BlogPost is imported
 import Header from './components/Header';
